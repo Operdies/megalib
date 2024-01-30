815486aeb8f49d9c53f928f2c81cdcc8db9e8b9e
alex <alexnlarsen@gmail.com>
2024-01-30 08:38:36 +0100

    Small cleanup

--- hashset/hashset.h
+++ hashset/hashset.h
@@ -73,12 +73,16 @@ size_t hashset_strcmp(const hashset_key a, const hashset_key b);
 const size_t coefficients[] = { 1073741827, 1073741831, 1073741833, 1073741839, 1073741843, 1073741857, 1073741891, 1073741909, 1073741939, 1073741953, 1073741969, 1073741971, 1073741987, 1073741993, 1073742037, 1073742053, 1073742073, 1073742077, 1073742091, 1073742113, 1073742169, 1073742203, 1073742209, 1073742223, 1073742233, 1073742259, 1073742277, 1073742289, 1073742343, 1073742353, 1073742361, 1073742391, 1073742403, 1073742463, 1073742493, 1073742517, 1073742583, 1073742623, 1073742653, 1073742667, 1073742671, 1073742673, 1073742707, 1073742713, 1073742721, 1073742731, 1073742767, 1073742773, 1073742811, 1073742851, 1073742853, 1073742881, 1073742889, 1073742913, 1073742931, 1073742937, 1073742959, 1073742983, 1073743007, 1073743037, 1073743049, 1073743051, 1073743079, 1073743091, 1073743093, 1073743123, 1073743129, 1073743141, 1073743159, 1073743163, 1073743189, 1073743199, 1073743207, 1073743243, 1073743291, 1073743303, 1073743313, 1073743327, 1073743331, 1073743337, 1073743381, 1073743387, 1073743393, 1073743397, 1073743403, 1073743417, 1073743421, 1073743427, 1073743457, 1073743459, 1073743469, 1073743501, 1073743507, 1073743513, 1073743543, 1073743577, 1073743591, 1073743633, 1073743739, 1073743757, };
 const size_t n_coefficients = sizeof(coefficients) / sizeof(coefficients[0]);
 
+inline size_t hash(size_t value) {
+  return value * 2;
+}
+
 size_t hash_integer(hashset_key key) {
-  return key.integer * 2;
+  return hash(key.integer);
 }
 
 size_t hash_pointer(hashset_key key) {
-  return key.integer / 4;
+  return hash(key.integer / 8);
 }
 
 size_t hash_string(hashset_key key) {
@@ -141,9 +145,8 @@ void destroy_hashset(hashset *h) {
 
 bool hashset_contains_key(const hashset *h, const hashset_key key, size_t *index) {
   if (h->count == 0) return false;
-  size_t hash, slot;
-  hash = h->hashfunc(key);
-  slot = hash % h->capacity;
+  size_t slot;
+  slot = h->hashfunc(key) % h->capacity;
   while (h->used[slot]) {
     if (h->cmpfunc(h->keys[slot], key) == 0) {
       if (index) 
@@ -166,9 +169,8 @@ bool hashset_get(const hashset *h, const hashset_key key, hashset_value *value) 
 }
 
 bool hash_insert(hashset *h, kvp_t kvp) {
-  size_t hash, slot;
-  hash = h->hashfunc(kvp.key);
-  slot = hash % h->capacity;
+  size_t slot;
+  slot = h->hashfunc(kvp.key) % h->capacity;
   while (h->used[slot]) {
     if (h->cmpfunc(h->keys[slot], kvp.key) == 0) {
       return false;
